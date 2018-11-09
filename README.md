22cac15e0e1c14ebb603d7c04d5af3a8fb244e6e
ExcelE <excelespina@gmail.com>
2018-11-09 00:36:02 -0500

    Added support for FoodNetwork

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Very basic scraper intended for scraping ingredients from recipes.
 
 Supported sites:  
-1) Allrecipes
+1) Allrecipes  
+2) FoodNetwork  
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const request = require('request-promise')
 const cheerio = require('cheerio')
 const fs = require('fs');
+const colors = require('colors/safe')
 
-const url = "https://www.allrecipes.com/recipe/268706/cranberry-sweet-potato-casserole/?internalSource=rotd&referringContentType=Homepage&clickId=cardslot%201"
-
+const url = "https://www.foodneasdasdtwork.com/recipes/tyler-florence/mashed-potatoes-recipe-1943035"
 
 function saver(json, name) {
     var jsoned = JSON.stringify(json, null, 4);
@@ -12,12 +12,47 @@ function saver(json, name) {
     });
 }
 
+function paradigmShift(url) {
+    const curr = {"list": [
+        {
+            "site": "allrecipes",
+            "title": "h1[itemprop=name]",
+            "ingredients": ".recipe-ingred_txt.added"
+        },
+        {
+            "site": "foodnetwork",
+            "title": ".o-AssetTitle__a-HeadlineText",
+            "ingredients": ".o-Ingredients__a-Ingredient"
+        }
+    ]};
+
+    var currSite = url.split('.').slice(1)[0];
+    var retJson = {};
+    curr.list.forEach(function(i, elem, arr) {
+        // Just to make sure that the ingredients are what comes out
+        if(i.site === currSite) {
+            retJson = i;
+        }
+    });
+
+    return retJson;
+}
+
 async function scraper(url, fileout = Date.now()) {
+
+    var checked = paradigmShift(url);
+    
+    if (Object.keys(checked).length === 0 && checked.constructor === Object) {
+        console.error(colors.bgRed("Website currently not supported!"));
+        process.exit(-1);
+    }
+
     try {
         var recipe = {
             recipeName: "",
             ingredients: [],
         }
+
         // Scrape
         const htmlResult = await request.get(url);
         // HTML Tag selector:
@@ -26,18 +61,18 @@ async function scraper(url, fileout = Date.now()) {
             decodeEntities: false 
         });
 
-        recipe.recipeName = $('h1[itemprop=name]').text();
+        recipe.recipeName = $(checked.title).text();
 
         // store into ingredients array
-        $('.recipe-ingred_txt.added').each(function(i, elem, arr) {
+        $(checked.ingredients).each(function(i, elem, arr) {
             // Just to make sure that the ingredients are what comes out
             if($(this).text().substring(0,3) !== 'Add') {
                 recipe.ingredients.push($(this).html());
             }
         });
-        var name = recipe.recipeName.split(' ')[0];
-
-        saver(recipe, name + '-' + fileout);
+        var name = recipe.recipeName.split(' ').slice(0,2).join('-');
+        var timestamp = fileout.toString().slice(-5,-1);
+        saver(recipe, checked.site + '-' +name + '-' + timestamp);
 
     } catch (err) {
         console.error(err);
