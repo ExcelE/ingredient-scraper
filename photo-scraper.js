5229a53cab32818464698ab612870a6cfbb7d371
ExcelE <excelespina@gmail.com>
2018-11-10 13:36:43 -0500

    List of car make and models

--- photo-scraper.js
+++ photo-scraper.js
@@ -1,34 +1,45 @@
 var gis = require('g-i-s');
 var download = require('image-downloader')
 const fs = require('fs-extra')
-const { GoogleImageSearch } = require('free-google-image-search')
 
-searchTitle = '2006 ABT Audi Allroad Quattro';
+searchTitle = '2017 Tesla Model X';
 
-// gis(searchTitle, logResults);
+gis(searchTitle, logResults);
 
-GoogleImageSearch.searchImage("cats")
-.then((res) => {
-    console.log(res); // This will return array of image URLs
-})
-
-async function dirPlanner(folder) {
+function dirPlanner(folder) {
 	var newPath = folder.replace(/\s+/g, '-').toLowerCase();
 	try {
-		await fs.ensureDir(newPath);
+		fs.ensureDir(newPath);
 		console.log('success!');
 	} catch (err) {
 		console.error(err);
+		process.exit(-1);
 	}
+	return newPath;
 }
 
-dirPlanner(searchTitle);
+var pathName = dirPlanner(searchTitle);
 
 function fileFormatChecker(filename) {
 	if (filename.split('.').reverse()[0] !== 'jpg'){
 		return false
 	}
 	return true
+}
+
+function imageDownloader(image) {
+	try {
+		download.image(image)
+		.then(({ filename, image }) => {
+			console.log('File saved to', filename)
+		})
+		.catch((err) => {
+			console.error(err)
+		})
+	}
+	catch (err) {
+		console.error(err);
+	}
 }
 
 async function logResults(error, results) {
@@ -41,19 +52,13 @@ async function logResults(error, results) {
 			if(fileFormatChecker(i.url)){
 				const options = {
 					url: i.url,
-					dest: __dirname + '/cat/' + i.url.split('/').reverse()[0].trim(),
+					dest: __dirname + `/${pathName}/` + i.url.split('/').reverse()[0].trim(),
 				};
 				urlList.push(options);
 		}
 		})
 		urlList.forEach(function(i, elem) {
-		download.image(i)
-		.then(({ filename, image }) => {
-			console.log('File saved to', filename)
-		})
-		.catch((err) => {
-			console.error(err)
-		})
+			imageDownloader(i);
 		})
 	}
 	
