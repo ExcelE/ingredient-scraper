7646246ed2c3b6e1d1021a32bf240a89ca62342e
ExcelE <excelespina@gmail.com>
2018-11-11 10:08:17 -0500

    Working scraper with delays in place

--- photo-scraper.js
+++ photo-scraper.js
@@ -1,18 +1,16 @@
-var gis = require('g-i-s');
-var download = require('image-downloader')
+const gis = require('g-i-s');
+const download = require('image-downloader')
 const fs = require('fs-extra')
-var shuffle = require('shuffle-array')
+const shuffle = require('shuffle-array')
 const delay = require('delay')
-
-
-let rawData = fs.readFileSync('carsBrand-28.json');
-var cars = JSON.parse(rawData);
+const rawData = fs.readFileSync('carsBrand-28.json');
+const cars = JSON.parse(rawData);
 
 (async() => {
-	for(let i = 0; i < 10; i++) {
+	for(let i = 0; i < 1; i++) {
 		var carArray = cars.brands[i];
 		var searchTitle = Object.keys(carArray).toString();
-		await delay(500);
+		await delay(5000);
 		currCar = dirPlanner(searchTitle);
 		search(carArray, currCar)
 	}
@@ -53,38 +51,44 @@ function imageDownloader(image) {
 	}
 }
 
+async function scrapeAndDownload(key, dirName) {
+	gis(key, async (error, results) => {
+		if (error) {
+			console.log(error);
+		} else {
+			if (results.length === 0) {
+				console.log('No search results for', key)
+				return
+			}
+			// Create array of 100 random indexes from results to download images from
+			const indexes = Array.from({ length: 100 }, () => Math.floor(Math.random() * results.length))
+			for (const num of indexes) {
+				const i = results[num]
+				if (!fileFormatChecker(i.url)) continue;
+				await delay(200);
+				imageDownloader({
+					url: i.url,
+					dest: __dirname + `/cars/${dirName}/` + i.url.split('/').reverse()[0].trim(),
+				})
+			}
+		}
+	})
+}
 
-	
-
-function search (query, dirName) {
+async function search (query, dirName) {
 	var keywords = query;
-	for(var feature in keywords) {
+	// Per brand
+	for (var feature in keywords) {
 		modelArray = keywords[feature];
-		modelArray.forEach(function(key) {
-			gis(key, (error, results) => {
-				if (error) {
-					console.log(error);
-					}
-					else {
-						if (results.length === 0) console.log('No search results for', query)
-						var urlList = [];
-						results.forEach(function(i, elem) {
-							if(fileFormatChecker(i.url)){
-								const options = {
-									url: i.url,
-									dest: __dirname + `/cars/${dirName}/` + i.url.split('/').reverse()[0].trim(),
-								};
-								urlList.push(options);
-						}})
-						shuffle(urlList);
-						for(let i = 0; i < 100; i++){
-							imageDownloader(urlList[i]);
-						}
-					}
-				}
-			)
-		})
+		if (!Array.isArray(modelArray)) {
+			 console.log('Array not found for', feature)
+			continue
+		}
+		// Per car
+		for(let i = 0; i < modelArray.length; i++){
+			await delay(500);
+			scrapeAndDownload(modelArray[i], dirName);
+		}
 		
 	}
-	
 }
