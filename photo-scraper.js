eec4a9c952f15cd8759ed3a087531c2095541ae1
ExcelE <excelespina@gmail.com>
2018-11-11 08:46:08 -0500

    Adding more params because we need more data.

--- photo-scraper.js
+++ photo-scraper.js
@@ -1,29 +1,29 @@
 var gis = require('g-i-s');
 var download = require('image-downloader')
 const fs = require('fs-extra')
+var shuffle = require('shuffle-array')
+const delay = require('delay')
 
-let rawData = fs.readFileSync('carsList.json');
+
+let rawData = fs.readFileSync('carsBrand-28.json');
 var cars = JSON.parse(rawData);
 
-for(let i = 0; i < 5; i++) {
-	searchTitle = cars.carsList[i];
-	console.log(searchTitle);
-	currCar = dirPlanner(searchTitle);
-	gis(searchTitle, (error, results) => logResults(error, results, currCar));
-}
-
-// cars.carsList.forEach(element => {
-// 	searchTitle = element;
-// 	console.log(searchTitle);
-// 	currCar = dirPlanner(searchTitle);
-// 	gis(searchTitle, (error, results) => logResults(error, results, searchTitle));
-// });
+(async() => {
+	for(let i = 0; i < 10; i++) {
+		var carArray = cars.brands[i];
+		var searchTitle = Object.keys(carArray).toString();
+		await delay(500);
+		currCar = dirPlanner(searchTitle);
+		search(carArray, currCar)
+	}
+}) ();
 
 
 function dirPlanner(folder) {
 	var newPath = folder.replace(/\s+/g, '-').toLowerCase();
+	// console.log(newPath)
 	try {
-		fs.ensureDir(`./cars/${newPath}`);
+		fs.ensureDirSync(`./cars/${newPath}`);
 		// console.log('success!');
 	} catch (err) {
 		console.error(err);
@@ -38,34 +38,53 @@ function fileFormatChecker(filename) {
 }
 
 function imageDownloader(image) {
-	download.image(image)
-	.then(({ filename, image }) => {
-		console.log('File saved to', filename)
-	})
-	.catch((err) => {
-		console.error(err)
-	})
+	// Check for file existance
+	try {
+		var exists = fs.statSync(image.dest);
+	}
+	catch(err) {
+		download.image(image)
+		.then(({ filename, image }) => {
+			console.log('File saved to', filename)
+		})
+		.catch((err) => {
+			console.error(err)
+		})
+	}
 }
 
-async function logResults(error, results, query) {
-	if (error) {
-	console.log(error);
-	}
-	else {
-		if (results.length === 0) console.log('No search results for', query)
-		var urlList = [];
-		results.forEach(function(i, elem) {
-			if(fileFormatChecker(i.url)){
-				const options = {
-					url: i.url,
-					dest: __dirname + `/cars/${query}/` + i.url.split('/').reverse()[0].trim(),
-				};
-				urlList.push(options);
-		}})
+
+	
+
+function search (query, dirName) {
+	var keywords = query;
+	for(var feature in keywords) {
+		modelArray = keywords[feature];
+		modelArray.forEach(function(key) {
+			gis(key, (error, results) => {
+				if (error) {
+					console.log(error);
+					}
+					else {
+						if (results.length === 0) console.log('No search results for', query)
+						var urlList = [];
+						results.forEach(function(i, elem) {
+							if(fileFormatChecker(i.url)){
+								const options = {
+									url: i.url,
+									dest: __dirname + `/cars/${dirName}/` + i.url.split('/').reverse()[0].trim(),
+								};
+								urlList.push(options);
+						}})
+						shuffle(urlList);
+						for(let i = 0; i < 100; i++){
+							imageDownloader(urlList[i]);
+						}
+					}
+				}
+			)
+		})
 		
-		for(let i = 0; i < 30; i++){
-			imageDownloader(urlList[i]);
-		}
 	}
 	
 }
